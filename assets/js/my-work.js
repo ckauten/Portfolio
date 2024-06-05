15ffa421bf9d7d304d0ac5d0d5c972bc2f95066d
Cameron Kauten <cameron.kauten@gmail.com>
2024-06-05 10:27:18 -0700

    work tiles updated with gif backgrounds and formatting changed

--- assets/js/my-work.js
+++ assets/js/my-work.js
@@ -1,36 +1,38 @@
-document.addEventListener("DOMContentLoaded", checkDarkMode);
+document.addEventListener('DOMContentLoaded', checkDarkMode);
 
 //Global variables
-let toggleDarkMode = document.querySelector(".toggleDarkMode");
-let sun = document.querySelector(".fa-sun");
-let moon = document.querySelector(".fa-moon");
+let toggleDarkMode = document.querySelector('.toggleDarkMode');
+let sun = document.querySelector('.fa-sun');
+let moon = document.querySelector('.fa-moon');
 
 //THE DARKMODE SETTING
 
 function checkDarkMode() {
-  if (localStorage.getItem("darkmode") === "true") {
+  if (localStorage.getItem('darkmode') === 'true') {
     darkmode();
   } else {
     lightmode();
   }
 }
 
-moon.addEventListener("click", darkmode);
+moon.addEventListener('click', darkmode);
 
 function darkmode() {
-  toggleDarkMode.style.filter = "invert(100%)";
-  sun.style.visibility = "visible";
-  moon.style.visibility = "hidden";
-  localStorage.setItem("darkmode", "true");
+  toggleDarkMode.style.filter = 'invert(100%)';
+  sun.style.visibility = 'visible';
+  moon.style.visibility = 'hidden';
+  localStorage.setItem('darkmode', 'true');
+  document.querySelector('#projectContainer').style.filter = 'invert(100%)';
 }
 
 //THE LIGHTMODE SETTING
 
-sun.addEventListener("click", lightmode);
+sun.addEventListener('click', lightmode);
 
 function lightmode() {
-  toggleDarkMode.style.filter = "invert(0%)";
-  sun.style.visibility = "hidden";
-  moon.style.visibility = "visible";
-  localStorage.setItem("darkmode", "false");
+  toggleDarkMode.style.filter = 'invert(0%)';
+  sun.style.visibility = 'hidden';
+  moon.style.visibility = 'visible';
+  localStorage.setItem('darkmode', 'false');
+  document.querySelector('#projectContainer').style.filter = 'invert(0%)';
 }
